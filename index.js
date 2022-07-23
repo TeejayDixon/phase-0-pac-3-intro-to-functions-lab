feb236fdcf2f2b5115ceed93a32dab05f321d3fc
Teejay Dixon <tahjgdixon@gmail.com>
2022-07-23 03:27:53 -0700

    commit tree ea778f4d3a71bc9fdae75e6214485b6ec7902046

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+logShout("hello");
+
+function logShout(string) {
+
+  console.log(string.toUpperCase("hello"));
+
+}
+
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase()) {
+
+    return ["I can't hear you!"];
+
+  } else {
+    string.toUpperCase()
+    return [`YES INDEED!`];
+  }
+}
